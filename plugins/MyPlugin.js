5405a93338914f0feb5805e5697b302a3e1dab1d
shockwang <henryandjay@hotmail.com>
2019-06-27 15:20:38 +0800

    Add visible mechanism
    Add function: updateVisible
    1. add visible mechanism, player can only see fields not blocked by wall
    or ceiling.
    2. deal with ceiling followed by wall.
    3. deal with corner case.

--- plugins/MyPlugin.js
+++ plugins/MyPlugin.js
@@ -1,7 +1,7 @@
 // my plugin
 
 (function() {
-    MapData = function(floorId) {
+    MapData = function(floorId, original) {
         this.base = floorId;
         this.bush = 0;
         this.decorate1 = 0;
@@ -12,11 +12,19 @@
         // data added for explored/visible
         this.isExplored = false;
         this.isVisible = false;
+        
+        // original tile
+        this.originalTile = original;
     }
     
     MapVariable = function(mapData, rmDataMap) {
         this.mapData = mapData;
         this.rmDataMap = rmDataMap;
+    }
+    
+    Coordinate = function(x, y) {
+        this.x = x;
+        this.y = y;
     }
     
     rawDataOld = [
@@ -33,6 +41,10 @@
     var ceilingCenter = 5888;
     var wallCenter = 6282;
     var floorCenter = 2816;
+    var warFogCenter = 3536;
+    
+    // view parameters
+    var viewDistance = 5;
     
     MapUtils = function() {
         throw new Error('This is a static class');
@@ -42,11 +54,156 @@
         return Math.floor(Math.random() * Math.floor(max));
     };
     
-    function playerCanSee(x, y) {
-        if (Math.abs($gamePlayer._x - x) <= 1 && Math.abs($gamePlayer._y - y) <= 1) {
-            return true;
+    // this function should be called twice
+    function updateVisible(x, y, mapData) {
+        var visible = false;
+        if (Math.abs($gamePlayer._x - x) <= viewDistance && Math.abs($gamePlayer._y - y) <= viewDistance) {
+            // around player, check visibility
+            visible = true;
+            if ($gamePlayer._x != x) {
+                var path = [];
+                var m = ($gamePlayer._y - y) / ($gamePlayer._x - x);
+                var startX, endX, lastY;
+                if ($gamePlayer._x - x > 0) {
+                    startX = x;
+                    endX = $gamePlayer._x;
+                    lastY = y;
+                } else {
+                    startX = $gamePlayer._x;
+                    endX = x;
+                    lastY = $gamePlayer._y;
+                }
+
+                // start to check if vision blocked
+                for (var i = startX+1; i <= endX; i++) {
+                    var estimatedY = Math.round(m * (i - $gamePlayer._x) + $gamePlayer._y);
+                    
+                    // fill y-axis coordinates
+                    if (Math.abs(lastY - estimatedY) > 1) {
+                        var startY = (lastY - estimatedY > 0) ? estimatedY : lastY;
+                        var endY = (lastY - estimatedY > 0) ? lastY : estimatedY;
+                        for (var j = startY+1; j < endY; j++) {
+                            var estimatedX = Math.round((j - $gamePlayer._y) / m + $gamePlayer._x);
+                            path.push(new Coordinate(estimatedX, j));
+                        }
+                    }
+                    path.push(new Coordinate(i, estimatedY));
+                    lastY = estimatedY;
+                }
+                
+                for (var i = 0; i < path.length; i++) {
+                    // does not count the (x, y) point
+                    if (mapData[path[i].x][path[i].y].originalTile != FLOOR && !(path[i].x == x && path[i].y == y)) {
+                        visible = false;
+                        break;
+                    }
+                }
+            } else {
+                var start = ($gamePlayer._y - y > 0) ? y : $gamePlayer._y;
+                var end = ($gamePlayer._y - y > 0) ? $gamePlayer._y : y;
+                
+                // start to check if vision blocked
+                for (var i = start+1; i < end; i++) {
+                    if (mapData[x][i].originalTile != FLOOR) {
+                        visible = false;
+                        break;
+                    }
+                }
+            }
+            
+            // ceiling followed by wall check
+            if (visible && mapData[x][y].originalTile == WALL) {
+                mapData[x][y-1].isVisible = true;
+            }
+            
+            // other ceiling check
+            if (!visible && mapData[x][y].originalTile == CEILING) {
+                if (y+1 < mapData[0].length && mapData[x][y+1].originalTile == WALL && mapData[x][y+1].isVisible) {
+                    // ceiling attached with wall
+                    visible = true;
+                } else {
+                    var xVisible = false, yVisible = false;
+                    if ((x-1 >= 0 && mapData[x-1][y].isVisible) || (x+1 < mapData.length && mapData[x+1][y].isVisible)) {
+                        xVisible = true;
+                    }
+                    if ((y-1 >= 0 && mapData[x][y-1].isVisible) || (y+1 < mapData[0].length && mapData[x][y+1].isVisible)) {
+                        yVisible = true;
+                    }
+                    if (xVisible && yVisible) {
+                        // corner case
+                        visible = true;
+                    }
+                }
+            }
         }
-        return false;
+        
+        mapData[x][y].isVisible = visible;
+    }
+    
+    function refineMapTile(east, west, south, north, centerTile) {
+        var result = ceilingCenter;
+        if (east) {
+            if (west) {
+                if (north) {
+                    if (south) {
+                        result += 46;
+                    } else {
+                        result += 42;
+                    }
+                } else {
+                    if (south) {
+                        result += 44;
+                    } else {
+                        result += 32;
+                    }
+                }
+            } else {
+                if (north) {
+                    if (south) {
+                        result += 45;
+                    } else {
+                        result += 36;
+                    }
+                } else {
+                    if (south) {
+                        result += 38;
+                    } else {
+                        result += 24;
+                    }
+                }
+            }
+        } else {
+            if (west) {
+                if (north) {
+                    if (south) {
+                        result += 43;
+                    } else {
+                        result += 34;
+                    }
+                } else {
+                    if (south) {
+                        result += 40;
+                    } else {
+                        result += 16;
+                    }
+                }
+            } else {
+                if (north) {
+                    if (south) {
+                        result += 33;
+                    } else {
+                        result += 20;
+                    }
+                } else {
+                    if (south) {
+                        result += 28;
+                    } else {
+                        result += 0;
+                    }
+                }
+            }
+        }
+        return result;
     }
     
     MapUtils.translateMap = function(rawData){
@@ -54,7 +211,7 @@
         for (var i = 0; i < mapData.length; i++) {
             mapData[i] = new Array(rawData[0].length);
             for (var j = 0; j < rawData[0].length; j++) {
-                mapData[i][j] = new MapData(floorCenter);
+                mapData[i][j] = new MapData(floorCenter, rawData[i][j]);
             }
         }
         
@@ -192,11 +349,21 @@
         for (var i = 0; i < mapArray.length; i++) {
             mapArray[i] = 0;
         }
+        
+        // first time update visibility
+        for (var j = 0; j < mapData[0].length; j++) {
+            for (var i = 0; i < mapData.length; i++) {
+                updateVisible(i, j, mapData);
+            }
+        }
+        
         var index = 0;
         var shadowOffset = mapData.length * mapData[0].length * 4;
         for (var j = 0; j < mapData[0].length; j++) {
             for (var i = 0; i < mapData.length; i++) {
-                if (playerCanSee(i, j) || mapData[i][j].isExplored) {
+                // second time update visibility
+                updateVisible(i, j, mapData);
+                if (mapData[i][j].isVisible || mapData[i][j].isExplored) {
                     mapArray[index] = mapData[i][j].base;
                     mapArray[shadowOffset + index] = mapData[i][j].shadow;
                     mapData[i][j].isExplored = true;
@@ -379,7 +546,6 @@
     
     // override map loading mechanism
     DataManager.isMapLoaded = function() {
-        console.log("$dataMap: " + $dataMap + ", $gameMap: " + $gameMap);
         if ($gameMap && $gameMap.mapId() > 0) {
             if ($dataMap && !$gameVariables[$gameMap.mapId()]) {
                 // first load map
@@ -412,6 +578,8 @@
         Game_Character.prototype.moveStraight.call(this, d);
         if (moved) {
             MapUtils.drawMap($gameVariables[$gameMap.mapId()].mapData, $dataMap.data);
+            // add for steam RMMV project
+            setTimeout('SceneManager.goto(Scene_Map)', 250);
         }
     };
 
